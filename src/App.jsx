92919a167a565179d2579c60af3b3ed13d6f9090
Nishu singh <nishu.frontend@gmail.com>
2026-01-24 09:00:55 +0530

    Add todo functionality using React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
 
+  function handleAdd() {
+    const trimmed = text.trim();
+    if (!trimmed) return;
+
+    const newTodos = { text: trimmed, done: false };
+    setTodos([...todos, newTodos]);
+    setText("");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        placeholder="Enter todo"
+      />
+      <button onClick={handleAdd}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.done ? "☑️" : ""}
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default App
+export default App;
